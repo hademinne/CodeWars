6522021a855f636075155ef4ed8d01ada8992a9e
Hademinne <hademinne@gmail.com>
2023-09-22 11:13:58 +0000

    add new challenge

---
+++ 8kyu/oddOreven.js
@@ -0,0 +1,12 @@
+/*
+DESCRIPTION:
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+
+
+
+*/
+
+//Solution
+let evenOrOdd = (n) => (n % 2 ? "Odd" : "Even");
